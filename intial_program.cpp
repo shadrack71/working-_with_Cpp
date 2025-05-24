2d199fa4c668dcb2d25de633550e45f30aed156c
shadrack <shadrackkinsimba@gmail.com>
2025-05-25 02:28:11 +0300

    basic  output and input

---
+++ intial_program.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void myfunction()
+{
+    std::cout << "  am working in the projecst" << std::endl;
+}
+
+int main()
+{
+
+    std::cout << " 4hello world3";
+    std::cout << " 3hello world2" << std::endl;
+    std::cout << " 5hello world1" << std::endl;
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << i << std::endl;
+        // my name
+    }
+    myfunction();
+
+    return 0;
+}
