262c2cab98e9120188d17fce2599440091b48326
shadrack <shadrackkinsimba@gmail.com>
2025-05-25 02:50:22 +0300

    basic   statement and function

--- intial_program.cpp
+++ intial_program.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 
+using namespace std;
+
+// string userInput;
+
+string name = "John";
+int age = 35;
+double height = 6.1;
+const string nams = "shadrack";
+
+int x = 5, y = 6, z = 50;
+
+int myNumber{4};
+
 void myfunction()
 {
-    std::cout << "  am working in the projecst" << std::endl;
+    cout << "  am working in the projecst" << endl;
 }
 
 int main()
 {
 
-    std::cout << " 4hello world3";
-    std::cout << " 3hello world2" << std::endl;
-    std::cout << " 5hello world1" << std::endl;
+    cout << " 4hello world3" << endl;
+    cout << " 3hello world2" << endl;
+    cout << " 5hello world1" << endl;
+    // getline(std::cin, userInput);
     for (int i = 0; i < 10; i++)
     {
-        std::cout << i << std::endl;
+        cout << i << endl;
         // my name
     }
+
+    cout << name << " is " << age << " years old and " << height << " feet tall.";
+    // cout << userInput << endl;
     myfunction();
 
     return 0;
