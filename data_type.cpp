5e788c79bdadfd0c3c4d87b3e9330efb512264bf
shadrack <shadrackkinsimba@gmail.com>
2025-05-26 15:58:51 +0300

    woring  with data type

---
+++ data_type.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int myNum = 5;             // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+double myDoubleNum = 9.98; // Floating point number
+char myLetter = 'D';       // Character
+bool myBoolean = true;     // Boolean
+string myText = "Hello";   // String
+
+int main()
+{
+
+    cout << sizeof(myNum) << endl;
+
+    return 0;
+}
