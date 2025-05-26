3c711ba7fcbb1b935d28e1f069fb9a0acf7b1231
shadrack <shadrackkinsimba@gmail.com>
2025-05-26 16:12:41 +0300

    woring   Integer Modifiers

--- data_type.cpp
+++ data_type.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <limits>
 using namespace std;
 
 int myNum = 5;             // Integer (whole number)
@@ -7,12 +8,27 @@ float myFloatNum = 5.99;   // Floating point number
 double myDoubleNum = 9.98; // Floating point number
 char myLetter = 'D';       // Character
 bool myBoolean = true;     // Boolean
-string myText = "Hello";   // String
+string myText = "Hello";
+
+void IntegerModifiers()
+{
+    signed int myFod = 123;
+    short int shortNum = 32767;                      // Max value for short
+    unsigned int unsignedNum = 4294967295;           // Max value for unsigned int
+    long int longNum = 2147483647;                   // Max value for long
+    long long int longLongNum = 9223372036854775807; // Max for long long
+
+    std::cout << "Short int: " << shortNum << " (Size: " << sizeof(shortNum) << " bytes)" << std::endl;
+    std::cout << "Unsigned int: " << unsignedNum << " (Size: " << sizeof(unsignedNum) << " bytes)" << std::endl;
+    std::cout << "Long int: " << longNum << " (Size: " << sizeof(longNum) << " bytes)" << std::endl;
+    std::cout << "Long long int: " << longLongNum << " (Size: " << sizeof(longLongNum) << " bytes)" << std::endl;
+}
 
 int main()
 {
 
     cout << sizeof(myNum) << endl;
+    IntegerModifiers();
 
     return 0;
 }
